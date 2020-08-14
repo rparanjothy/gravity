9de9e2a9493d528f2e33c75c1f20f230a86bc10b
Ram Paranjothy <ram.paranjothy@amd.com>
2020-08-13 23:34:13 -0500

    midpoint

--- gears.js
+++ gears.js
@@ -1,16 +1,16 @@
 let slider;
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  slider = createSlider(0, 200, 150, 10);
+  slider = createSlider(0, 500, 150, 10);
   slider.position(10, 10);
   slider.style("width", "80px");
 
-  shaft = createSlider(0, 500, 100, 10);
+  shaft = createSlider(0, 500, 200, 10);
   shaft.position(10, 40);
   shaft.style("width", "80px");
 
-  g1 = new gear("pink", 100, new Vector(0, 0), 0, 0.1, 100);
-  g2 = new gear("red", 200, new Vector(150, 0), 0, -0.05, 400, 110);
+  // g1 = new gear("pink", 100, new Vector(0, 0), 0, 0.1, 100);
+  g2 = new gear("red", 200, new Vector(150, 0), 0, 0.05, 400, 110);
   // g3 = new gear(
   //   "green",
   //   50,
@@ -72,7 +72,8 @@ class gear {
     let a1 = Math.acos(cy / this.shaft);
     let x1 = Math.sin(a1) * this.shaft;
     // shaft
-    line(this.dotLoc.x + x1, 0, this.dotLoc.x, this.dotLoc.y);
+    let shaftEnd = new Vector(this.dotLoc.x + x1, 0);
+    line(shaftEnd.x, shaftEnd.y, this.dotLoc.x, this.dotLoc.y);
     // crank
     line(this.dotLoc.x, this.dotLoc.y, this.location.x, this.location.y);
 
@@ -86,6 +87,20 @@ class gear {
     //     new Vector(this.dotLoc.x + x1, 0)
     //   )
     // );
+
+    line(this.dotLoc.x + x1, 0, this.dotLoc.x + x1 + 250, 0);
+
+    // mid point of shaft
+    let dx = -shaftEnd.x + this.dotLoc.x;
+    let dy = -shaftEnd.y + this.dotLoc.y;
+    let ax = Math.atan(dy / dx);
+    strokeWeight(10);
+
+    point(
+      (this.shaft / 2) * Math.cos(ax) + this.dotLoc.x,
+      this.dotLoc.y + (this.shaft / 2) * Math.sin(ax)
+    );
+    strokeWeight(1);
   }
 }
 
